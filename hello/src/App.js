326607624ac6279af183bc650aff0186c3150f1a
HongDaeEui <aviate8@gmail.com>
2021-12-11 17:29:50 +0900

    Add: props basic

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,16 +2,10 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    hello: "hello react World!",
+    count: 0,
   };
 
   handleChange = () => {
-    this.setState({
-      hello: "Oh change messasge",
-    });
-  };
-
-  countUP = () => {
     this.setState({
       count: this.state.count + 1,
     });
@@ -20,10 +14,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>{this.state.hello}</div>;
-        <button onClick={this.handleChange}>click Me!</button>;
-        <div>{this.state.count}</div>
-        <button onClick={this.countUp}>count up !</button>
+        <div className="props">
+          {/* Props가 들어가는 부분입니다! */}
+          <span>{this.props.message}</span>
+        </div>
+
+        <div className="state">
+          {/* State가 들어가는 부분입니다! */}
+          {this.state.count}
+          <button onClick={this.handleChange}>click Me !</button>
+        </div>
       </div>
     );
   }
